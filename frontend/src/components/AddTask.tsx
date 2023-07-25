597f10fb3b6305c122c7c1f50258cb4ad1cdcc96
sc30gsw <haidoushi875@gmail.com>
2023-07-25 22:07:18 +0900

    タスク登録機能の実装

--- frontend/src/components/AddTask.tsx
+++ frontend/src/components/AddTask.tsx
@@ -5,16 +5,79 @@ import DialogActions from '@mui/material/DialogActions'
 import DialogContent from '@mui/material/DialogContent'
 import DialogTitle from '@mui/material/DialogTitle'
 import { useState } from 'react'
+import { useMutation } from '@apollo/client'
+import { Task } from '../types/task'
+import { CREATE_TASK } from '../mutations/taskMutations'
+import { GET_TASKS } from '../queries/taskQueries'
+import { useNavigate } from 'react-router-dom'
 
-const AddTask = () => {
+const AddTask = ({ userId }: { userId: number }) => {
   const [open, setOpen] = useState(false)
   const [name, setName] = useState('')
   const [dueDate, setDueDate] = useState('')
   const [description, setDescription] = useState('')
+  const [isInvalidName, setIsInvalidName] = useState(false)
+  const [isInvalidDueDate, setIsInvalidDueDate] = useState(false)
+
+  const [createTask] = useMutation<{ createTask: Task }>(CREATE_TASK)
+
+  const navigate = useNavigate()
+
+  const resetState = () => {
+    setName('')
+    setDueDate('')
+    setDescription('')
+    setIsInvalidName(false)
+    setIsInvalidDueDate(false)
+  }
+
+  const handleAddTask = async () => {
+    let canAdd = true
+
+    if (name.length === 0) {
+      canAdd = false
+      setIsInvalidName(true)
+    } else {
+      setIsInvalidName(false)
+    }
+
+    if (!Date.parse(dueDate)) {
+      canAdd = false
+      setIsInvalidDueDate(true)
+    } else {
+      setIsInvalidDueDate(false)
+    }
+
+    if (canAdd) {
+      const createTaskInput = { name, dueDate, description, userId }
+      try {
+        await createTask({
+          variables: { createTaskInput },
+          // 再発行したいQueryを指定する(登録後、一覧取得する)
+          refetchQueries: [{ query: GET_TASKS, variables: { userId } }],
+        })
+
+        resetState()
+        setOpen(false)
+      } catch (err: any) {
+        if (err.message === 'Unauthorized') {
+          localStorage.remove('token')
+          alert('トークンの有効期限が切れました。サインイン画面に遷移します。')
+          navigate('/signin')
+          return
+        }
+
+        alert('タスクの登録に失敗しました')
+      }
+    }
+  }
 
   const handleClickOpen = () => setOpen(true)
 
-  const handleClose = () => setOpen(false)
+  const handleClose = () => {
+    resetState()
+    setOpen(false)
+  }
 
   return (
     <div>
@@ -37,6 +100,8 @@ const AddTask = () => {
             required
             value={name}
             onChange={(e) => setName(e.target.value)}
+            error={isInvalidName}
+            helperText={isInvalidName && 'タスク名を入力してください'}
           />
           <TextField
             autoFocus
@@ -48,6 +113,8 @@ const AddTask = () => {
             required
             value={dueDate}
             onChange={(e) => setDueDate(e.target.value)}
+            error={isInvalidDueDate}
+            helperText={isInvalidDueDate && '日付形式で入力してください'}
           />
           <TextField
             autoFocus
@@ -63,7 +130,7 @@ const AddTask = () => {
         </DialogContent>
         <DialogActions>
           <Button onClick={handleClose}>Cancel</Button>
-          <Button onClick={handleClose}>Add</Button>
+          <Button onClick={handleAddTask}>Add</Button>
         </DialogActions>
       </Dialog>
     </div>
