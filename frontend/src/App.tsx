d10084d8f97522ff38ea9f1b583784f0e27afecb
sc30gsw <haidoushi875@gmail.com>
2023-07-25 19:20:02 +0900

    GraphQL・ApolloClientのセットアップ

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,17 +5,22 @@ import NotFound from './components/NotFound'
 import SignIn from './components/SignIn'
 import SignUp from './components/SignUp'
 import { GuestRoot, PrivateRoot } from './AuthRoot'
+import client from './apolloClient'
+import { ApolloProvider } from '@apollo/client'
 
 const App = () => {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/signin" element={<GuestRoot children={<SignIn />} />} />
-        <Route path="/signup" element={<GuestRoot children={<SignUp />} />} />
-        <Route path="/" element={<PrivateRoot children={<Main />} />} />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
-    </BrowserRouter>
+    // ApolloProviderで囲まれたコンポーネントからGraphQLクライアントを使用できるようになる
+    <ApolloProvider client={client}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/signin" element={<GuestRoot children={<SignIn />} />} />
+          <Route path="/signup" element={<GuestRoot children={<SignUp />} />} />
+          <Route path="/" element={<PrivateRoot children={<Main />} />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </ApolloProvider>
   )
 }
 
