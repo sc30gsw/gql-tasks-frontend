72b1032ba5fe2d40a536319373ac989ac95d83c6
sc30gsw <haidoushi875@gmail.com>
2023-07-25 19:10:19 +0900

    認証によるページ遷移の制御

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,21 +1,22 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import "./App.css";
-import Main from "./components/Main";
-import NotFound from "./components/NotFound";
-import SignIn from "./components/SignIn";
-import SignUp from "./components/SignUp";
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import './App.css'
+import Main from './components/Main'
+import NotFound from './components/NotFound'
+import SignIn from './components/SignIn'
+import SignUp from './components/SignUp'
+import { GuestRoot, PrivateRoot } from './AuthRoot'
 
 const App = () => {
-	return (
-		<BrowserRouter>
-			<Routes>
-				<Route path="/signin" element={<SignIn />} />
-				<Route path="/signup" element={<SignUp />} />
-				<Route path="/" element={<Main />} />
-				<Route path="*" element={<NotFound />} />
-			</Routes>
-		</BrowserRouter>
-	);
-};
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/signin" element={<GuestRoot children={<SignIn />} />} />
+        <Route path="/signup" element={<GuestRoot children={<SignUp />} />} />
+        <Route path="/" element={<PrivateRoot children={<Main />} />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
+  )
+}
 
-export default App;
+export default App
